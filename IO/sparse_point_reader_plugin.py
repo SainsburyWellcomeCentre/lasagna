8cb2f5bb9bcf686170cecd634bce32d25f8b1a1c
Rob Campbell <git@raacampbell.com>
2016-11-09 16:53:54 +0100

    Allow the sparse point reader plugin to accept a fourth column that is used to segregate data into different point series. Each series is plotted as a separate ingredient. This is explained in the plugin's doc text.

--- IO/sparse_point_reader_plugin.py
+++ IO/sparse_point_reader_plugin.py
@@ -2,13 +2,31 @@
 """
 Read sparse points from a text file.
 
-The expected format of the sparse points file:
-The text file containing the sparse points should be in the form:
+The sparse points file makes it possible to load individual point data into the 3D space defined 
+the volume image. 
+
+The sparse points file may have one of two formats.
+In both cases it's one row per data point. 
+There is no header that contains information on what the columns are.
+
+ONE:
 z_position,x_position,y_position\n
 z_position,x_position,y_position\n
 ...
 
-No header corresponding to column names. 
+
+TWO
+z_position,x_position,y_position,data_series_number\n
+z_position,x_position,y_position,data_series_number\n
+...
+
+
+In the second format, each data point is associated with a scalar value.
+All points with the same scalar value are grouped together as one ingredient. 
+This allows points of different sorts to be overlaid easily on the same 
+image and have their properties changed together. 
+
+
 """
 
 import os
@@ -71,16 +89,61 @@ class loaderClass(lasagna_plugin):
                     continue
                 data.append([float(x) for x in asList[ii].split(',')])
 
+            # A point series should be a list of lists where each list has a length of 3,
+            # corresponding to the position of each point in 3D space. However, point
+            # series could also have a length of 4. If this is the case, the fourth 
+            # value is the index of the series. This allows a single file to hold multiple
+            # different point series. We handle these two cases differently. First we deal
+            # with the the standard case:
+            if len(data[1]) == 3:
+                # Create an ingredient with the same name as the file name 
+                objName=fname.split(os.path.sep)[-1]
+                self.lasagna.addIngredient(objectName=objName, 
+                            kind=self.kind, 
+                            data=np.asarray(data), 
+                            fname=fname
+                            )
 
-            objName=fname.split(os.path.sep)[-1]
-            self.lasagna.addIngredient(objectName=objName, 
-                        kind=self.kind, 
-                        data=np.asarray(data), 
-                        fname=fname
-                        )
+                # Add this ingredient to all three plots
+                self.lasagna.returnIngredientByName(objName).addToPlots() 
 
-            self.lasagna.returnIngredientByName(objName).addToPlots() #Add item to all three 2D plots
-            self.lasagna.initialiseAxes()
+                # Update the plots
+                self.lasagna.initialiseAxes()
+
+            elif len(data[1]) == 4:
+                # What are the unique data series values?
+                dSeries = [x[3] for x in data]
+                dSeries = list(set(dSeries))
+                
+                # Loop through these unique series and add as separate sparse point objects
+
+                for thisIndex in dSeries:
+                    tmp = []
+                    for thisRow in data:
+                        if thisRow[3]==thisIndex:
+                            tmp.append(thisRow[:3])
+
+                    print "Adding point series %d with %d points" % (thisIndex,len(tmp))
+
+                    # Create an ingredient with the same name as the file name 
+                    objName = "%s #%d" % (fname.split(os.path.sep)[-1],thisIndex)
+
+                    self.lasagna.addIngredient(objectName=objName, 
+                                kind=self.kind, 
+                                data=np.asarray(tmp), 
+                                fname=fname
+                                )
+
+                    # Add this ingredient to all three plots
+                    self.lasagna.returnIngredientByName(objName).addToPlots() 
+
+                    # Update the plots
+                    self.lasagna.initialiseAxes()
+
+
+            else:
+                print("Point series has %d columns. Only 3 or 4 columns are supported" % len(data[1]))
+
 
         else:
             self.lasagna.statusBar.showMessage("Unable to find " + str(fname))
