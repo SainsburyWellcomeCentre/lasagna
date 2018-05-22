0179490610ce97b777d4e9818389a3b047038f5a
Charly Rousseau <c.rousseau@ucl.ac.uk>
2018-05-22 12:19:07 +0100

    Refactors imageProcessing

--- lasagna/imageProcessing/coreFunctions.py
+++ lasagna/imageProcessing/coreFunctions.py
@@ -9,18 +9,18 @@ the main main.py file and without bloating it much.
 import numpy as np
 
 
-def defaultHistRange(y,x):
-	"""
-	Returns a reasonable values for the maximum plotted value.
-	y - range of values from the intensity histogram
-	x - range of x values for the histogram
-	"""
+def defaultHistRange(y, x):
+    """
+    Returns a reasonable values for the maximum plotted value.
+    y - range of values from the intensity histogram
+    x - range of x values for the histogram
+    """
 
-	#I'm sure this isn't the most robust approach but it works for now
-	thresh=0.925 #find values greater than this proportion
+    # I'm sure this isn't the most robust approach but it works for now
+    thresh = 0.925  # find values greater than this proportion
 
-	m = x*y
-	vals = np.cumsum(m)/np.sum(m)
-	vals = vals>thresh
+    m = x*y
+    vals = np.cumsum(m) / np.sum(m)
+    vals = vals > thresh
 
-	return x[vals.tolist().index(True)]
+    return x[vals.tolist().index(True)]
