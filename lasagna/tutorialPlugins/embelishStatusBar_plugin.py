f1cc2535c5600ea8fdd0fe90641cc18bcd29bb39
Charly Rousseau <c.rousseau@ucl.ac.uk>
2018-05-22 14:29:44 +0100

    Fixes imports and refactors locals of tutorialPlugins

--- lasagna/tutorialPlugins/embelishStatusBar_plugin.py
+++ lasagna/tutorialPlugins/embelishStatusBar_plugin.py
@@ -3,20 +3,17 @@
 """
 Embelishes the lasagna status bar in a pointless way
 """
-from lasagna_plugin import lasagna_plugin
+from lasagna.lasagna_plugin import lasagna_plugin
+
 
 class plugin(lasagna_plugin):
 
-    def __init__(self,lasagna):
-        super(plugin,self).__init__(lasagna) #Run the constructor from lasagna_plugin with lasagna as an input argument
-
-
-        #re-define some default properties that were originally defined in lasagna_plugin
-        self.pluginShortName='Embelisher' #Appears on the menu
-        self.pluginLongName='status bar emblishment' #Can be used for other purposes (e.g. tool-tip)
-        self.pluginAuthor='Rob Campbell'
-
-
+    def __init__(self, lasagna):
+        super(plugin, self).__init__(lasagna)  # Run the constructor from lasagna_plugin with lasagna as an input argument
+        # re-define some default properties that were originally defined in lasagna_plugin
+        self.pluginShortName = 'Embelisher'  # Appears on the menu
+        self.pluginLongName = 'status bar emblishment'  # Can be used for other purposes (e.g. tool-tip)
+        self.pluginAuthor = 'Rob Campbell'
     """
     Define a special "hook_XXX" method. This method is run at the end of lasagna.updateStatusBar()
     right before the message is actually displayed. In general, hooks should be in the form
@@ -28,6 +25,3 @@ class plugin(lasagna_plugin):
 
     def closePlugin(self):
         self.detachHooks()
-        
-
-   
--- lasagna/tutorialPlugins/overlayRandomPoints_plugin.py
+++ lasagna/tutorialPlugins/overlayRandomPoints_plugin.py
@@ -13,66 +13,61 @@ point data onto the plot axes. The plugin does the following:
 
 """
 
-from lasagna_plugin import lasagna_plugin
 import numpy as np
-import sys
+
+from lasagna.lasagna_plugin import lasagna_plugin
+
 
 class plugin(lasagna_plugin): 
 
-    def __init__(self,lasagna,parent=None):
-        super(plugin,self).__init__(lasagna)
+    def __init__(self, lasagna, parent=None):
+        super(plugin, self).__init__(lasagna)
 
-        #re-define some default properties that were originally defined in lasagna_plugin
+        # re-define some default properties that were originally defined in lasagna_plugin
         self.pluginShortName='Overlay random points' 
         self.pluginLongName='displays dynamically generated random points onto the axes' 
         self.pluginAuthor='Rob Campbell'
 
         self.lasagna = lasagna
 
-        #add a sparsepoints ingredient
+        # add a sparsepoints ingredient
         self.objName = 'tutePoints'
-        self.lasagna.addIngredient(objectName=self.objName , 
-                                   kind='sparsepoints' ,
+        self.lasagna.addIngredient(objectName=self.objName,
+                                   kind='sparsepoints',
                                    data=self.generateRandomData())
 
-        self.lasagna.returnIngredientByName(self.objName).addToPlots() #Add item to all three 2D plots
+        self.lasagna.returnIngredientByName(self.objName).addToPlots()  # Add item to all three 2D plots
         self.lasagna.axes2D[0].listNamedItemsInPlotWidget()
-        self.lasagna.initialiseAxes(resetAxes=True) #update the plots.
-
+        self.lasagna.initialiseAxes(resetAxes=True)  # update the plots.
 
     def generateRandomData(self):
         """
         Generate random data spanning the extent of the first image stack
         """
-        stackName = self.lasagna.imageStackLayers_Model.index(0,0).data()
-        firstLayer = self.lasagna.returnIngredientByName(stackName)
-        if firstLayer == False:
+        stack_name = self.lasagna.imageStackLayers_Model.index(0, 0).data()
+        first_layer = self.lasagna.returnIngredientByName(stack_name)
+        if not first_layer:
             print("No image layers loaded")
             return
 
-        imShape = firstLayer.data().shape
+        im_shape = first_layer.data().shape
 
-        n = 10000 #number of random points to make
-        numAxes = 3
+        n = 10000  # number of random points to make
+        num_axes = 3
 
-        r = np.random.rand(n*numAxes).reshape((n, numAxes)) #the random array
+        r = np.random.rand(n*num_axes).reshape((n, num_axes))  # the random array
 
-        #multiply values in each dimension of the array by the array size of the first layer image
-        for ii in range(numAxes):
-            r[:,ii] = np.floor(r[:,ii]*imShape[ii])
+        # multiply values in each dimension of the array by the array size of the first layer image
+        for i in range(num_axes):
+            r[:, i] = np.floor(r[:, i]*im_shape[i])
 
         return r
 
-
-
-    #The following methods are involved in shutting down the plugin window
+    # The following methods are involved in shutting down the plugin window
     """
     This method is called by lasagna when the user unchecks the plugin in the menu
     """
 
     def closePlugin(self):
-        #Remove points from axes then remove the ingredient from the list. 
+        # Remove points from axes then remove the ingredient from the list.
         self.lasagna.removeIngredientByName(self.objName)
-
-
-
