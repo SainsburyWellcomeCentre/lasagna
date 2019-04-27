3022672b509620ad3257f9ab2b6d9594d8c49d5f
Antonin Blot <antonin.blot@gmail.com>
2019-04-27 16:37:32 +0100

    Add a loader window to load and downscale sparsepoint files

--- compileGUI.sh
+++ compileGUI.sh
@@ -2,3 +2,4 @@
 pyuic5 ./designerFiles/lasagna_mainWindow.ui > ./lasagna_mainWindow.py
 pyrcc5 ./designerFiles/mainWindow.qrc >  ./mainWindow_rc.py
 pyuic5 ./designerFiles/alert.ui > ./alert_UI.py
+pyuic5 ./designerFiles/loader_dialog.ui > ./loader_dialog_UI.py
