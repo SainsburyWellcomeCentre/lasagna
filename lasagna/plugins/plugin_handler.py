630a43543200cbade8351a4c3529b9fa50ae29a8
Rob Campbell <git@raacampbell.com>
2019-06-23 22:18:21 +0100

    Don't crash if the plugin path has a trailing slash

--- lasagna/plugins/plugin_handler.py
+++ lasagna/plugins/plugin_handler.py
@@ -18,6 +18,8 @@ def find_plugins(plugin_paths):
             print("Plugin path {} is not a valid path. SKIPPING".format(plugin_folder))
             continue
 
+        plugin_folder = plugin_folder.rstrip(os.sep)
+
         # Get all files in the directory that match the plugin file name pattern
         plugin_files = [f for f in os.listdir(plugin_folder) if is_plugin_file(plugin_folder, f)]
         if not plugin_files:
@@ -47,7 +49,7 @@ def get_plugin_instance_from_file_name(file_name, attribute_to_import='plugin'):
         print(' * Could not load plugin module {}. Skipping.'.format(module_name))
         ## TODO: uncomment following to see error, but then loading does not proceed. That's odd,
         ## because it should just report the error. This is a bug, TBH, shouldn't have to uncomment.
-        print(err.__class__.__name__ + ": " + err.message) 
+        #print(err.__class__.__name__ + ": " + err.message) 
         return None, None
     if attribute_to_import is not None:
         returned_attribute = getattr(imported_module, attribute_to_import)
